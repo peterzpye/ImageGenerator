5fd2f5ab9944c7ef13bca0599a707bd0ef37dff2
Zhipeng Ye <yzp6038@gmail.com>
2022-11-14 23:11:29 -0800

    added requirements.txt and setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='ImageGeneration',
+    version='beta',
+    packages=['ImageGeneration'],
+    install_requires=['torch', 'torchvision'],
+    url='',
+    license='',
+    author='yezhipen',
+    author_email='',
+    description=''
+)
